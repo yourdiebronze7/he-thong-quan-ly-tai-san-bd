7979ebd347a99196e434bc4a426651b1c1248618
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-05 11:21:20 -0700

    feat: initial commit for truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+// Replace this with your own mnemonic and Infura/Alchemy endpoint
+const mnemonic = 'your mnemonic here';
+const infuraUrl = 'https://rinkeby.infura.io/v3/your-infura-project-id';
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*', // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraUrl),
+      network_id: 4,
+      gas: 5500000,
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0"
+    }
+  }
+};
