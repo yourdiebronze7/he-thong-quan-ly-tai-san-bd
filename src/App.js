1611eb10dd8ca6bcd6e931775251b335f0460e1b
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-05 11:21:13 -0700

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hệ Thống Quản Lý Tài Sản Bất Động Sản</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
