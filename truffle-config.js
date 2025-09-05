43a8fdf1525e58039098ae6af6792da12c0de3e9
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-05 11:21:45 -0700

    Update mnemonic placeholder for better clarity.

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-// Replace this with your own mnemonic and Infura/Alchemy endpoint
-const mnemonic = 'your mnemonic here';
+// Replace this with your own mnemonic phrase and Infura/Alchemy endpoint
+const mnemonic = 'YOUR_MNEMONIC_HERE';
 const infuraUrl = process.env.INFURA_URL || 'https://rinkeby.infura.io/v3/your-infura-project-id';
 
 module.exports = {
